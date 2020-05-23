a8f4c7fc8743fc3d627b2499f49e07f226e46584
EsanEm <62729091+EsanEm@users.noreply.github.com>
2020-05-23 15:42:10 +0100

    Enable camel notation for API output

--- StudentNotes/App_Start/WebApiConfig.cs
+++ StudentNotes/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace StudentNotes
 {
@@ -9,6 +11,12 @@ namespace StudentNotes
     {
         public static void Register(HttpConfiguration config)
         {
+            // To enable camel notation for API output
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
